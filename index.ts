e94ce54179b0c540a16cd5ccb0b42c2cab56d09b
tim-hub <tim-hub@users.noreply.github.com>
2023-09-12 20:04:01 +1200

    bun ts init

---
+++ index.ts
@@ -0,0 +1,12 @@
+// @ts-ignore
+import { Reference } from './lib/reference';
+// @ts-ignore
+import { Range } from './lib/range';
+// @ts-ignore
+import { Books } from './lib/books';
+
+export default {
+  Reference,
+  Range,
+  Books,
+};
